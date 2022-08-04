2745030944d8cccec5901555915a7eff556af88d
bobdeng <bobdeng@163.com>
2022-08-04 17:37:12 +0800

    login

--- src/webapp/src/views/Login.vue
+++ src/webapp/src/views/Login.vue
@@ -3,7 +3,7 @@
     <Card title="管理员登录">
       <Form>
         <FormItem label="密码">
-          <InputPassword v-model:value="password" id="inputPassword" maxlength="20"/>
+          <InputPassword v-model:value="password" id="inputPassword" :maxlength="20"/>
         </FormItem>
         <FormItem>
           <Button type="primary" @click="login" id="buttonLogin">登录</Button>
@@ -17,11 +17,12 @@
 import {Card, Form, FormItem, InputPassword, Button} from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
 import {ref} from "vue";
+import {LoginForm, server} from "../HttpServer";
 
 const password = ref("")
 
 function login() {
-  password.value = ""
+  server.login(new LoginForm(password.value))
 }
 </script>
 
