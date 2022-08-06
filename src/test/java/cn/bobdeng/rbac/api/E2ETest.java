859b8f0ba9de0823c333ff9874538fe7c6324403
bobdeng <bobdeng@163.com>
2022-08-06 15:05:30 +0800

    feat:增加外部功能读取接口

--- src/test/java/cn/bobdeng/rbac/api/E2ETest.java
+++ src/test/java/cn/bobdeng/rbac/api/E2ETest.java
@@ -4,17 +4,22 @@ import cn.bobdeng.rbac.Cookies;
 import cn.bobdeng.rbac.api.pages.AdminLoginPage;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 public abstract class E2ETest {
     @Autowired
     WebDriverHandler webDriverHandler;
     @Autowired
     JdbcTemplate jdbcTemplate;
+    @Autowired
+    MockMvc mockMvc;
 
     protected void clearTable(String tableName) {
         jdbcTemplate.execute("truncate table " + tableName);
--- src/test/java/cn/bobdeng/rbac/api/FunctionsTest.java
+++ src/test/java/cn/bobdeng/rbac/api/FunctionsTest.java
@@ -3,12 +3,6 @@ package cn.bobdeng.rbac.api;
 import cn.bobdeng.rbac.domain.function.Function;
 import com.google.gson.Gson;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
 import java.nio.charset.StandardCharsets;
@@ -16,15 +10,10 @@ import java.nio.charset.StandardCharsets;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
-public class FunctionsTest {
-    @Autowired
-    MockMvc mockMvc;
+public class FunctionsTest extends E2ETest {
+
 
     @Test
     public void should_list_functions() throws Exception {
