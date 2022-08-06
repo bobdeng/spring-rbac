15dc2852d44d5ffb1b6e295d98fe3b08bad4a045
bobdeng <bobdeng@163.com>
2022-08-06 16:05:39 +0800

    feat: add field check

--- src/main/java/cn/bobdeng/rbac/api/BaseController.java
+++ src/main/java/cn/bobdeng/rbac/api/BaseController.java
@@ -1,14 +1,16 @@
 package cn.bobdeng.rbac.api;
 
+import cn.bobdeng.rbac.domain.FieldChecker;
+import cn.bobdeng.rbac.domain.FieldIllegalException;
 import cn.bobdeng.rbac.security.PermissionDeniedException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RestControllerAdvice
@@ -19,6 +21,12 @@ public class BaseController {
         response.setContentType("application/json;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
         response.getWriter().println("无权限");
+    }
+
+    @ExceptionHandler(FieldIllegalException.class)
+    public List<FieldChecker.FieldError> onInputError(FieldIllegalException e, HttpServletResponse response) {
+        response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+        return e.getErrors();
     }
 
     @ExceptionHandler(RuntimeException.class)
