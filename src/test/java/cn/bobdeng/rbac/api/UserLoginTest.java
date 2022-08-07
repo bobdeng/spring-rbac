1867aa23a4220350ea66f3607203b95a36bb1eec
bobdeng <bobdeng@163.com>
2022-08-07 16:55:20 +0800

    feat: 普通用户登录

--- src/test/java/cn/bobdeng/rbac/api/UserLoginTest.java
+++ src/test/java/cn/bobdeng/rbac/api/UserLoginTest.java
@@ -35,4 +35,13 @@ public class UserLoginTest extends E2ETest {
         loginPage.open();
         assertTrue(loginPage.hasText("租户1"));
     }
+
+    @Test
+    public void should_login_success() {
+        AdminLoginPage loginPage = new AdminLoginPage(webDriverHandler);
+        loginPage.open();
+        loginPage.loginWith("bobdeng", "123456");
+        loginPage.waitUntilNoButtonSpin();
+        assertTrue(WebDriverHandler.WEBDRIVER.getCurrentUrl().endsWith("#/user/console"));
+    }
 }
