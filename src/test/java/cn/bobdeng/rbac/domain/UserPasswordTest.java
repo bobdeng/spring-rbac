d99867ee815ef0df554cc0226bd45a8ebd25a976
bobdeng <bobdeng@163.com>
2022-08-20 10:58:37 +0800

    test: fix test

--- src/test/java/cn/bobdeng/rbac/domain/UserPasswordTest.java
+++ src/test/java/cn/bobdeng/rbac/domain/UserPasswordTest.java
@@ -13,26 +13,30 @@ import static org.mockito.Mockito.*;
 
 public class UserPasswordTest {
     private RbacContext rbacContext;
+    private User.UserPassword userPassword;
+    private User user;
 
     @BeforeEach
     public void setup() {
         rbacContext = mock(RbacContext.class);
-    }
-
-    @Test
-    public void save_user_password() {
-        User.UserPassword userPassword = mock(User.UserPassword.class);
-        User user = new User(1, null);
+        userPassword = mock(User.UserPassword.class);
+        user = new User(1, new UserDescription("", User.UserStatus.Normal));
         user.setRbacContext(rbacContext);
         user.setTenant(Tenant::new);
         ConfigurationContext configurationContext = mock(ConfigurationContext.class);
         Parameters parameters = mock(Parameters.class);
         user.setConfigurationContext(configurationContext);
-        when(userPassword.encodePassword("123456")).thenReturn("654321");
+        User.UserLock userLock = mock(User.UserLock.class);
         when(parameters.findByIdentity(BaseParameters.PASSWORD_POLICY)).thenReturn(Optional.of(new Parameter("", new ParameterDescription("", "none"))));
         when(configurationContext.parameters(any())).thenReturn(parameters);
+        when(rbacContext.userLock(user)).thenReturn(userLock);
+        when(userLock.findByIdentity(user.identity())).thenReturn(Optional.empty());
         when(rbacContext.userPassword(user)).thenReturn(userPassword);
+    }
 
+    @Test
+    public void save_user_password() {
+        when(userPassword.encodePassword("123456")).thenReturn("654321");
         user.savePassword(new RawPassword("123456"));
 
         Password password = new Password(1, new PasswordDescription("654321"));
@@ -41,26 +45,17 @@ public class UserPasswordTest {
 
     @Test
     public void should_return_false_when_user_password_not_exist() {
-        User.UserPassword userPassword = mock(User.UserPassword.class);
-        when(userPassword.match("123456", "654321")).thenReturn(true);
-        PasswordDescription description = new PasswordDescription("654321");
         when(userPassword.findByIdentity(1)).thenReturn(Optional.empty());
-        User user = new User(1, null);
-        user.setRbacContext(rbacContext);
-        when(rbacContext.userPassword(user)).thenReturn(userPassword);
         assertFalse(user.verifyPassword("123456"));
     }
 
     @Test
     public void verify_user_password() {
-        User.UserPassword userPassword = mock(User.UserPassword.class);
-        when(userPassword.match("123456", "654321")).thenReturn(true);
         PasswordDescription description = new PasswordDescription("654321");
         when(userPassword.findByIdentity(1)).thenReturn(Optional.of(new Password(1, description)));
-        User user = new User(1, null);
-        user.setRbacContext(rbacContext);
-        when(rbacContext.userPassword(user)).thenReturn(userPassword);
+        when(userPassword.match("123456","654321")).thenReturn(true);
         assertTrue(user.verifyPassword("123456"));
+        when(userPassword.match("123455","654321")).thenReturn(false);
         assertFalse(user.verifyPassword("123455"));
     }
 }
