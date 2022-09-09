827a22e4226e27700101b4d701a9c3bad7419ec1
bobdeng <bobdeng@163.com>
2022-09-09 11:30:43 +0800

    feat: add from to login

--- src/webapp/src/views/Home.vue
+++ src/webapp/src/views/Home.vue
@@ -5,16 +5,21 @@
 </template>
 
 <script setup lang="ts">
-import {useRouter} from "vue-router";
+import {useRouter, useRoute} from "vue-router";
 import Login from "./Login.vue";
 
 const router = useRouter();
+const route = useRoute()
 const onAdminLogin = () => {
   router.replace({
     name: "console",
   });
 };
 const onUserLogin = () => {
+  if (route.query.from) {
+    document.location.href = route.query.from as string
+    return;
+  }
   router.replace({
     name: "userConsole",
   });
